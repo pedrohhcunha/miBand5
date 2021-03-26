68da31bea142da79c3c9b95dca5de6d5b4541b8d
Pedro Henrique Hoffmann da Cunha <63168567+pedrohhcunha@users.noreply.github.com>
2021-03-26 11:32:01 -0300

    responsive

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const span = document.querySelector('#span')
-
+const btnMenu = document.querySelector('#menuBtn')
+const header = document.querySelector('.header')
 window.onscroll = () => {
     if(window.scrollY > 0){
         span.classList.add('active')
     } else {
         span.classList.remove('active')
     }
+    header.classList.remove('open')
 }
+btnMenu.addEventListener('click', () => {
+    header.classList.toggle('open')
+})
